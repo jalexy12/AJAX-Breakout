55d65926199274ce113fbd16d6a87b613eb938dc
festus-sulumeti <sulumetifestus@outlook.com>
2024-07-21 14:35:18 +0300

    main.js

--- main.js
+++ main.js
@@ -7,43 +7,56 @@ $(document).ready(function(){
 		event.preventDefault();
 		
 		// Grab information from form
-		var name = $("#name").val();
-		var occupation = $("#occupation").val();
-		var weapon = $("#weapon").val();
+		var name = $("#name").val().trim();
+		var occupation = $("#occupation").val().trim();
+		var weapon = $("#weapon").val().trim();
+
+		if (!name || !occupation || !weapon) {
+			alert("Please fill out all fields.");
+			return;
+		}
+
 		// Create my "params" hash
-		// params[:name]
 		var params = {
 			name: name,
 			occupation: occupation,
 			weapon: weapon
 		};
+
 		// Make my AJAX request
 		$.ajax({
 			type: "POST",
 			url: "https://ironhack-characters.herokuapp.com/characters",
 			data: params,
-			success: updateList,
+			success: function(response) {
+				updateList();
+				// Clear form fields
+				$(".js-character-form")[0].reset();
+			},
 			error: characterError
-		})
-	})
-})
+		});
+	});
+});
 
 function getCharactersFromApi(){
 	$.ajax({
 		url: "https://ironhack-characters.herokuapp.com/characters",
 		success: showCharacters,
 		error: characterLoadError
-	})
+	});
 }
 
 function showCharacters(response){
-	response.forEach(function(character){
-		appendCharacter(character);
-	})
+	if (response.length === 0) {
+		$(".js-character-list").html("<li>No characters found.</li>");
+	} else {
+		response.forEach(function(character){
+			appendCharacter(character);
+		});
+	}
 }
 
 function appendCharacter(character){
-	// HTML Template String
 	var html = `
 		<li>
 			<b>Name:</b> ${character.name}<br>
@@ -52,21 +65,19 @@ function appendCharacter(character){
 		</li>
 	`;
 	$(".js-character-list").append(html);
-
 }
 
 function characterLoadError(err){
-	console.log("Error", err);
+	console.log("Error loading characters", err);
 }
 
-function updateList(response){
+function updateList(){
 	// Empty the current list
-	$(".js-character-list").empty()
+	$(".js-character-list").empty();
 	// Get the characters
-	// Append all of the characters again
 	getCharactersFromApi();
 }
 
 function characterError(err){
-	console.log("Error", err)
+	console.log("Error submitting character", err);
 }
